1c9d4e08985ea7564f975e267a21ec79d2325fc6
Юлия <kornyakova_11@mail.ru>
2024-08-05 14:49:57 +0500

    Add files via upload

---
+++ Организация программ.py
@@ -0,0 +1,7 @@
+# ДЗ урок 4 Организация программ module_1_4.py
+my_string = input("Вести название:")
+print(my_string.upper())
+print(my_string.lower())
+print("Вести название:".replace(" ", ""))
+print(my_string[1])
+print(my_string[-1])
